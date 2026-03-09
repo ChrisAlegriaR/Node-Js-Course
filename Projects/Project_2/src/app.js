f9e6c3f8e50f68fa37a8a1c02757d943d1af9e05
Chris Alegria <christian.alegriar@gmail.com>
2026-03-09 16:27:04 -0600

    Finished: Project 2

--- Projects/Project_2/src/app.js
+++ Projects/Project_2/src/app.js
@@ -10,6 +10,6 @@ const main = () => {
 
 // *Funcion agnóstica async autocovocada.
 // *Agnóstica por que no tiene nombre y se encuentra dentro de parentesis, async por que es una funcion de tipo async y autoconvocada por que la ejecutamos con los parentesis finales. 
-(async() => {
+(async() => { 
     main();
 })()
--- Projects/Project_2/src/server/server.js
+++ Projects/Project_2/src/server/server.js
@@ -6,7 +6,17 @@ const startServer = (options) => { // *Creamos una variable que va a almacenar u
     console.log(port); // *Imprime en consola el valor "port".
     console.log(public_path) // *Imprime en consola el valor "public_path".
 
-    const app = express();
+    const app = express(); // *Inicializa Express() detro de una variable llamada "app".
+    app.use(express.static(public_path)); // *Utilizamos un Middleware para las rutas en general, donde hacemos publica y estatica la capeta que almacenamos en public_path, esto implica que los archivos dentro de esa carpeta pueden ser servidos directamente al navegador
+
+        app.get(/.*/, (req, res) => {//*Establecemos una ruta, donde mediante /.*/ establecemos que cuando ninguna ruta anterior coincide dentro del servidor ejecute lo que se define en el handler.
+            const indexPath = path.join(__dirname + `../../../${public_path}/index.html`); // *Creamos una variable la cual, almacenara una direccion de carpetas, donde __dirname especifica en la ruta la ubicacion de este archivo en este caso src/server, y que integre a la path lo demas. quedando como....
+        res.sendFile(indexPath); // *Posteriormente usando el path que definimos retornaremos o mostraremos el archivo index.html 
+    });
+
+    app.listen(port, () => { // *Ejecutamos el servidor donde se crea un listener para ejecutar y escuchar todas las rutas.
+        console.log(`Escuchando en el puerto: ${port}`) // *Mensaje en consola que indica donde se esta ejecutando el servidor.
+    });
 };
 
 module.exports = { // *Mediante module.exports exportamos el contenido para poder ser utilizado/leido desde otros archivos.
