45796715a6eec7730156ccb2e5d03e137974d794
Chris Alegria <christian.alegriar@gmail.com>
2026-03-21 15:02:24 -0600

    Updated: DB Conection And Routes Declarations

--- Projects/Project_3/src/app.js
+++ Projects/Project_3/src/app.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import envs from './config/env.js'
 import mongoose from 'mongoose';
-import { router } from './routes/book.route.js';
+import { router as book_router} from './routes/book.route.js';
+import bodyParser from 'body-parser';
 
 // *Declaracion de app con express(), para ejecutar rutas y levantar el servidor.
 const app = express();
@@ -11,11 +12,15 @@ app.get(/.*/, (req, res) => {
     res.send('<h1>¡Servidor funcionando perfectamente!</h1>');
 })
 
-app.use('/book', router);
+// *Parseo de los bodies.
+app.use(bodyParser.json())
 
-// ~Se definen las rutas de "Book".
-// *Se define ruta Get que extrae los libros desde book.
-// app.use('/book', book_get);
+// *Conección a MongoDB.
+// mongoose.connect(envs.mongo_url, {dbName: envs.mongo_db});
+const db = mongoose.connection;
+
+// *Declaracion de rutas books.
+app.use('/books', book_router);
 
 // *Se define y realiza el levantamiento del servidor en un puerto.
 app.listen(envs.port,() => {
--- Projects/Project_3/src/config/env.js
+++ Projects/Project_3/src/config/env.js
@@ -3,6 +3,8 @@ config();
 
 const envs = {
     port: process.env.PORT || 3000,
+    mongo_url: process.env.MONGO_URL,
+    mongo_db: process.env.MONGO_DB
 }
 
 export default envs;
