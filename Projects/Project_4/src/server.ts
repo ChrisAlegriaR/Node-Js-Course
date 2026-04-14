3114cd1d205129f6d4b7a0452a72b36c65cdba46
Chris Alegria <christian.alegriar@gmail.com>
2026-04-14 13:57:05 -0600

    Updated: Main Server Console's Log

--- Projects/Project_4/src/server.ts
+++ Projects/Project_4/src/server.ts
@@ -4,5 +4,5 @@ import variables from "./config/env.js";
 
 // *Declaracion del iniciaador del servidor, siendo este el que levantara el mismo.
 app.listen(variables.port, () => {
-    console.log(`El puerto se esta ejecutando correctamente en el puerto ${variables.port}`);
+    console.log(`El servidor se esta ejecutando correctamente en el puerto ${variables.port}`);
 })
