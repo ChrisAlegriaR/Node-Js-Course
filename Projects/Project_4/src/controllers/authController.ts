765792b1f089fbc1182f0c536bf9bc1a17b0a45f
Chris Alegria <christian.alegriar@gmail.com>
2026-04-14 17:15:02 -0600

    Added: Prisma Schemas And Conections To Project 4

--- Projects/Project_4/src/controllers/authController.ts
+++ Projects/Project_4/src/controllers/authController.ts
@@ -1,5 +1,5 @@
 import { type Request, type Response } from "express"
 
-export const register = async(req: Request, res: Response) => {
-
+export const register = async(req: Request, res: Response): Promise<void> => {
+    const { email, password } = req.body;
 }
