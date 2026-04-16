e5b99fb2228275d2609657fc36af48180579bbd1
Chris Alegria <christian.alegriar@gmail.com>
2026-04-16 17:38:57 -0600

    Updated: Password And Email Process In Project 4

--- Projects/Project_4/src/controllers/authController.ts
+++ Projects/Project_4/src/controllers/authController.ts
@@ -1,10 +1,22 @@
-import { type Request, type Response } from "express"
+import { type Request, type Response } from "express";
+import { hashPassword } from "../services/password.service";
+import User from "../models/user"
 
 export const register = async(req: Request, res: Response): Promise<void> => {
     const { email, password } = req.body;
 
-    try{
-        const hashPassword: String = ''
+    try{    
+        const hashedPassword = await hashPassword(password);   
+        console.log(hashedPassword);
+        
+        const user = await User.create(
+            {
+                data: {
+                    email,
+                    password: hashedPassword
+                }
+            }
+        )
     } catch(error){
 
     }
